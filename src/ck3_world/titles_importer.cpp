e743ab3ef461c5c468676bbc53ca50a98c1daae0
Idhrendur <idhrendur@hotmail.com>
2026-07-03 16:10:33 -0700

    Log number of imported titles.

--- src/ck3_world/titles_importer.cpp
+++ src/ck3_world/titles_importer.cpp
@@ -1,10 +1,13 @@
 #include "src/ck3_world/titles_importer.hpp"
 
 #include <external/commonItems/CommonRegexes.h>
+#include <external/commonItems/Log.h>
 #include <external/commonItems/ModLoader/ModFilesystem.h>
 #include <external/commonItems/Parser.h>
 #include <external/commonItems/ParserHelpers.h>
+#include <fmt/format.h>
 
+#include <cstdio>
 #include <filesystem>
 #include <istream>
 #include <map>
@@ -19,6 +22,7 @@ namespace ck3_world
 std::map<std::filesystem::path, std::vector<std::string>> ImportTitles(const commonItems::ModFilesystem& ck3_root)
 {
    std::map<std::filesystem::path, std::vector<std::string>> titles;
+   size_t num_titles = 0;
 
    std::vector<std::string> titles_in_file;
 
@@ -34,9 +38,11 @@ std::map<std::filesystem::path, std::vector<std::string>> ImportTitles(const com
       titles_in_file.clear();
 
       file_parser.parseFile(file);
+      num_titles += titles_in_file.size();
       titles.emplace(file.filename(), titles_in_file);
    }
 
+   Log(LogLevel::Info) << fmt::format("Imported {} titles in {} files.", num_titles, titles.size());
    return titles;
 }
 
